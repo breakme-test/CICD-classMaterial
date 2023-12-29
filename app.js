cc37dfe652fe90dc77ce7f7db52f0b8f11085933
Justin Heath <just.coder@outlook.com>
2023-12-29 16:01:09 +0000

    updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-updateBG = () => {
-  const x = document.querySelector("body").style.backgroundColor;
-  document.querySelector("body").style.backgroundColor = (x === "lightgoldenrodyellow") ? "azure" : "lightgoldenrodyellow";
+const updateBG = () => {
+  const x = document.querySelector('body').style.backgroundColor
+  document.querySelector('body').style.backgroundColor =
+    x === 'lightgoldenrodyellow' ? 'azure' : 'lightgoldenrodyellow'
 }
 
-document.querySelector("#myBtn").addEventListener("click", updateBG);
+document.querySelector('#my-btn').addEventListener('click', updateBG)
