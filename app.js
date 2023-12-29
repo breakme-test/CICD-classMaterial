2bd66abe7a6255e6d76d4f472423259b402c2431
Justin Heath <just.coder@outlook.com>
2023-12-29 15:11:20 +0000

    error

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 updateBG = () => {
   const x = document.querySelector("body").style.backgroundColor;
   document.querySelector("body").style.backgroundColor = (x === "lightgoldenrodyellow") ? "azure" : "lightgoldenrodyellow";
-}
 
 document.querySelector("#myBtn").addEventListener("click", updateBG);
