b1668b9558ae28cae006620382837488960eead2
Justin Heath <just.coder@outlook.com>
2023-12-31 15:41:43 +0000

    final

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const updateBG = () => {
+// Function to change colour
+const updateBackgroundColour = () => {
   const x = document.querySelector('body').style.backgroundColor
   document.querySelector('body').style.backgroundColor =
     x === 'lightgoldenrodyellow' ? 'azure' : 'lightgoldenrodyellow'
 }
 
-document.querySelector('#my-btn').addEventListener('click', updateBG)
+module.exports = updateBackgroundColour
